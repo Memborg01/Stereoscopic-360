2466061956f50c46cb4d103482c38e9e0dfc4bb9
Memborg01 <jakmemborg@gmail.com>
2016-09-30 13:38:57 +0200

    Update 30/09 -2016

--- Assets/Project/Scripts/SceneManager.cs
+++ Assets/Project/Scripts/SceneManager.cs
@@ -17,9 +17,10 @@ public class SceneManager : MonoBehaviour {
         LeftCam.GetComponent<Camera>();
 
 
-
-        LeftCam.cullingMask = 8;
-        CamRight.cullingMask = 9;
+        LeftCam.cullingMask = 9 << 8;
+        CamRight.cullingMask = 9 << 9;
+        //LeftCam.cullingMask = 8;
+        //CamRight.cullingMask = 9;
 
         Debug.Log("Left Cull = " + LeftCam.cullingMask);
         Debug.Log("Right Cull = " + CamRight.cullingMask);
@@ -29,10 +30,11 @@ public class SceneManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (LeftCam.cullingMask != 9){
+      /*  if (LeftCam.cullingMask != 8){
 
             LeftCam.cullingMask = 9 << 8;
             //Debug.Log("Left Mask changed");
+            Debug.Log("Left Cull = " + LeftCam.cullingMask);
 
         }
         if (CamRight.cullingMask != 9)
@@ -40,8 +42,9 @@ public class SceneManager : MonoBehaviour {
 
             CamRight.cullingMask = 9 << 9;
             //Debug.Log("Right Mask changed");
+            Debug.Log("Right Cull = " + CamRight.cullingMask);
 
-        }
+        }*/
 
     }
 }
