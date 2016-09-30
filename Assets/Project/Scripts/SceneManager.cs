3681e701c824a0d15e2b09e5b87d7f57b8ff9e09
Memborg01 <jakmemborg@gmail.com>
2016-09-30 12:09:13 +0200

    360 setup added

---
+++ Assets/Project/Scripts/SceneManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class SceneManager : MonoBehaviour {
+
+    public Camera LeftCam;
+    public Camera CamRight;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        CamRight.GetComponent<Camera>();
+        LeftCam.GetComponent<Camera>();
+
+
+
+        LeftCam.cullingMask = 8;
+        CamRight.cullingMask = 9;
+
+        Debug.Log("Left Cull = " + LeftCam.cullingMask);
+        Debug.Log("Right Cull = " + CamRight.cullingMask);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (LeftCam.cullingMask != 9){
+
+            LeftCam.cullingMask = 9 << 8;
+            //Debug.Log("Left Mask changed");
+
+        }
+        if (CamRight.cullingMask != 9)
+        {
+
+            CamRight.cullingMask = 9 << 9;
+            //Debug.Log("Right Mask changed");
+
+        }
+
+    }
+}
