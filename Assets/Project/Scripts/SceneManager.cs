c85c841d63b6808ce42c9de20b31693d2126e1b1
Memborg01 <jakmemborg@gmail.com>
2016-10-03 12:38:22 +0200

    Fixed another Culling Mask issue and improved Google Cardboard support

--- Assets/Project/Scripts/SceneManager.cs
+++ Assets/Project/Scripts/SceneManager.cs
@@ -30,21 +30,21 @@ public class SceneManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-      /*  if (LeftCam.cullingMask != 8){
+        if (LeftCam.cullingMask != 256){
 
-            LeftCam.cullingMask = 9 << 8;
+            LeftCam.cullingMask = 1 << 8;
             //Debug.Log("Left Mask changed");
             Debug.Log("Left Cull = " + LeftCam.cullingMask);
 
         }
-        if (CamRight.cullingMask != 9)
+        if (CamRight.cullingMask != 512)
         {
 
-            CamRight.cullingMask = 9 << 9;
+            CamRight.cullingMask = 1 << 9;
             //Debug.Log("Right Mask changed");
             Debug.Log("Right Cull = " + CamRight.cullingMask);
 
-        }*/
+        }
 
     }
 }
